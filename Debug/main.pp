16cc8d65286fe7e9dd3af13b42eee361e2dbea17
Denis_S <SevastyanovDV@tom.transneft.ru>
2015-04-29 11:55:42 +0600

    Перенос проекта в Code Composer Studio v6 Шаг 1 - создание проекта, конфигурация проекта

---
+++ Debug/main.pp
@@ -0,0 +1,165 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/config.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/device_modification.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_rtos.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_queue.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/std.h
+main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/stdlib.h
+main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/linkage.h
+main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/stdlibf.h
+main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/string.h
+main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/math.h
+main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/float.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_utils.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_io.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_spi.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_sci.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_analog.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_i2c.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_memory.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_timer.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_dlog.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_Device.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_Adc.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_DevEmu.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_CpuTimers.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_ECan.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_ECap.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_EPwm.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_EQep.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_Gpio.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_I2c.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_PieCtrl.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_PieVect.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_Spi.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_Sci.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_SysCtrl.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_XIntrupt.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_GlobalPrototypes.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_ePwm_defines.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_I2C_defines.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/IQmathLib.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/timings.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/structs.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/macro.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/hardware.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/f280xbmsk.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/serial_communication.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/btwt41.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/interface.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/LogEv.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/menu2x.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/menu_global.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/key_codes.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/value_conv.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/rtc.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/VlvDrvCtrl.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/Calibs.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/im.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/rim_devices.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/at25xxx.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/display.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/displhal.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/dac7513.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/encoder.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/adt7301.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/ds3234.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/pult.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/Inputs.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/motor_control.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/ileg.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/dmc/sifu_calc.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/ApFilter1.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/ApFilter3.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/protections.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/PrtCalc.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/drv_modbus.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/responses.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/params.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/system.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/protectionI2T.h
+
+../main.c: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/config.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/device_modification.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_rtos.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_queue.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/std.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/stdlib.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/linkage.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/stdlibf.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/string.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/math.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/float.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_utils.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_io.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_spi.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_sci.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_analog.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_i2c.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_memory.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_timer.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_dlog.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_Device.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_Adc.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_DevEmu.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_CpuTimers.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_ECan.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_ECap.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_EPwm.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_EQep.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_Gpio.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_I2c.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_PieCtrl.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_PieVect.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_Spi.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_Sci.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_SysCtrl.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_XIntrupt.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_GlobalPrototypes.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_ePwm_defines.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_I2C_defines.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/IQmathLib.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/timings.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/structs.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/macro.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/hardware.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/f280xbmsk.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/serial_communication.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/btwt41.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/interface.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/LogEv.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/menu2x.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/menu_global.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/key_codes.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/value_conv.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/rtc.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/VlvDrvCtrl.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/Calibs.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/im.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/rim_devices.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/at25xxx.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/display.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/displhal.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/dac7513.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/encoder.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/adt7301.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/ds3234.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/pult.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/Inputs.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/motor_control.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/ileg.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/dmc/sifu_calc.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/ApFilter1.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/ApFilter3.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/protections.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/PrtCalc.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/drv_modbus.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/responses.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/params.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/system.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/protectionI2T.h: 
