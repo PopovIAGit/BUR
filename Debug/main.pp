6f2d6ca561322ceb8add04b6bf2e7d75deeae24c
Denis_S <SevastyanovDV@tom.transneft.ru>
2015-04-29 12:35:41 +0600

    Перенос проекта в Code Composer Studio v6 Шаг 2 - группировка файлов по категориям

--- Debug/main.pp
+++ Debug/main.pp
@@ -6,7 +6,7 @@ main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/device_modification.h
 main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl.h
 main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_rtos.h
 main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_queue.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/std.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/std.h
 main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/stdlib.h
 main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/linkage.h
 main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/stdlibf.h
@@ -42,45 +42,45 @@ main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_GlobalProtot
 main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_ePwm_defines.h
 main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_I2C_defines.h
 main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/IQmathLib.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/timings.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/structs.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/macro.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/hardware.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/timings.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/structs.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/macro.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/hardware.h
 main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/f280xbmsk.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/serial_communication.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/btwt41.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/interface.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/LogEv.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/menu2x.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/menu_global.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/key_codes.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/value_conv.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/rtc.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/VlvDrvCtrl.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/Calibs.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/im.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/rim_devices.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/at25xxx.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/display.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/displhal.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/dac7513.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/encoder.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/adt7301.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/ds3234.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/pult.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/Inputs.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/motor_control.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/ileg.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/dmc/sifu_calc.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/ApFilter1.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/ApFilter3.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/protections.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/PrtCalc.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/drv_modbus.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/responses.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/params.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/system.h
-main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/protectionI2T.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/comm/serial_communication.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/comm/btwt41.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/interface.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/stat/LogEv.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/menu2x.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/menu_global.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/key_codes.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/value_conv.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/rtc.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/VlvDrvCtrl.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/Calibs.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/stat/im.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/rim_devices.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/stat/at25xxx.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/display.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/displhal.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/dac7513.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/encoder.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/adt7301.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/ds3234.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/pult.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/comm/Inputs.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/motor_control.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/ileg.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/sifu_calc.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/ApFilter1.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/ApFilter3.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/protections.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/PrtCalc.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/comm/drv_modbus.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/comm/responses.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/params.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/system.h
+main.obj: C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/protectionI2T.h
 
 ../main.c: 
 C:/Users/SevastyanovDV/Desktop/Bur_1/include/config.h: 
@@ -88,7 +88,7 @@ C:/Users/SevastyanovDV/Desktop/Bur_1/include/device_modification.h:
 C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl.h: 
 C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_rtos.h: 
 C:/Users/SevastyanovDV/Desktop/Bur_1/include/csl/csl_queue.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/std.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/std.h: 
 c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/stdlib.h: 
 c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/linkage.h: 
 c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/stdlibf.h: 
@@ -124,42 +124,42 @@ C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_GlobalPrototypes.h:
 C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_ePwm_defines.h: 
 C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/DSP280x_I2C_defines.h: 
 C:/Users/SevastyanovDV/Desktop/Bur_1/include/IQmathLib.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/timings.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/structs.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/macro.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/hardware.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/timings.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/structs.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/macro.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/hardware.h: 
 C:/Users/SevastyanovDV/Desktop/Bur_1/include/chip/f280xbmsk.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/serial_communication.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/btwt41.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/interface.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/LogEv.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/menu2x.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/menu_global.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/key_codes.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/value_conv.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/rtc.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/VlvDrvCtrl.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/Calibs.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/im.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/rim_devices.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/at25xxx.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/display.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/displhal.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/dac7513.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/encoder.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/adt7301.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/ds3234.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/pult.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/Inputs.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/motor_control.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/ileg.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/dmc/sifu_calc.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/ApFilter1.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/ApFilter3.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/protections.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/PrtCalc.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/drv_modbus.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/responses.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/params.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/system.h: 
-C:/Users/SevastyanovDV/Desktop/Bur_1/include/protectionI2T.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/comm/serial_communication.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/comm/btwt41.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/interface.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/stat/LogEv.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/menu2x.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/menu_global.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/key_codes.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/value_conv.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/rtc.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/VlvDrvCtrl.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/Calibs.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/stat/im.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/rim_devices.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/stat/at25xxx.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/display.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/displhal.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/dac7513.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/encoder.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/adt7301.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/ds3234.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/pult.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/comm/Inputs.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/motor_control.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/ileg.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/sifu_calc.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/ApFilter1.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/peref/ApFilter3.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/protections.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/PrtCalc.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/comm/drv_modbus.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/comm/responses.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/params.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/global/system.h: 
+C:/Users/SevastyanovDV/Desktop/Bur_1/include/core/protectionI2T.h: 
