a273c41edb4b6da0d83d6797fef30b3971b66c2f
Ivan <PopovIA@tom.transneft.ru>
2015-05-22 08:40:41 +0600

    Коммит перед стягиванием2

---
+++ Debug/main.pp
@@ -0,0 +1,165 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/config.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/device_modification.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/csl/csl.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/csl/csl_rtos.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/csl/csl_queue.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/global/std.h
+main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/stdlib.h
+main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/linkage.h
+main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/stdlibf.h
+main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/string.h
+main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/math.h
+main.obj: c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/float.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/csl/csl_utils.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/csl/csl_io.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/csl/csl_spi.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/csl/csl_sci.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/csl/csl_analog.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/csl/csl_i2c.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/csl/csl_memory.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/csl/csl_timer.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/csl/csl_dlog.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_Device.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_Adc.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_DevEmu.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_CpuTimers.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_ECan.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_ECap.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_EPwm.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_EQep.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_Gpio.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_I2c.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_PieCtrl.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_PieVect.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_Spi.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_Sci.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_SysCtrl.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_XIntrupt.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_GlobalPrototypes.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_ePwm_defines.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_I2C_defines.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/IQmathLib.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/global/timings.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/global/structs.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/global/macro.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/global/hardware.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/chip/f280xbmsk.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/comm/serial_communication.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/comm/btwt41.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/global/interface.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/stat/LogEv.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/core/menu2x.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/core/menu_global.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/key_codes.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/core/value_conv.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/rtc.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/core/VlvDrvCtrl.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/Calibs.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/stat/im.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/rim_devices.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/stat/at25xxx.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/display.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/displhal.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/dac7513.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/encoder.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/adt7301.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/ds3234.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/pult.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/comm/Inputs.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/core/motor_control.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/ileg.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/core/sifu_calc.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/ApFilter1.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/peref/ApFilter3.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/core/protections.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/core/PrtCalc.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/comm/drv_modbus.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/comm/responses.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/core/params.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/global/system.h
+main.obj: C:/D/SOFT/BUR\ One/BUR/include/core/protectionI2T.h
+
+../main.c: 
+C:/D/SOFT/BUR\ One/BUR/include/config.h: 
+C:/D/SOFT/BUR\ One/BUR/include/device_modification.h: 
+C:/D/SOFT/BUR\ One/BUR/include/csl/csl.h: 
+C:/D/SOFT/BUR\ One/BUR/include/csl/csl_rtos.h: 
+C:/D/SOFT/BUR\ One/BUR/include/csl/csl_queue.h: 
+C:/D/SOFT/BUR\ One/BUR/include/global/std.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/stdlib.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/linkage.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/stdlibf.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/string.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/math.h: 
+c:/ti/ccsv6/tools/compiler/c2000_6.2.7/include/float.h: 
+C:/D/SOFT/BUR\ One/BUR/include/csl/csl_utils.h: 
+C:/D/SOFT/BUR\ One/BUR/include/csl/csl_io.h: 
+C:/D/SOFT/BUR\ One/BUR/include/csl/csl_spi.h: 
+C:/D/SOFT/BUR\ One/BUR/include/csl/csl_sci.h: 
+C:/D/SOFT/BUR\ One/BUR/include/csl/csl_analog.h: 
+C:/D/SOFT/BUR\ One/BUR/include/csl/csl_i2c.h: 
+C:/D/SOFT/BUR\ One/BUR/include/csl/csl_memory.h: 
+C:/D/SOFT/BUR\ One/BUR/include/csl/csl_timer.h: 
+C:/D/SOFT/BUR\ One/BUR/include/csl/csl_dlog.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_Device.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_Adc.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_DevEmu.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_CpuTimers.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_ECan.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_ECap.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_EPwm.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_EQep.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_Gpio.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_I2c.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_PieCtrl.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_PieVect.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_Spi.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_Sci.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_SysCtrl.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_XIntrupt.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_GlobalPrototypes.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_ePwm_defines.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/DSP280x_I2C_defines.h: 
+C:/D/SOFT/BUR\ One/BUR/include/IQmathLib.h: 
+C:/D/SOFT/BUR\ One/BUR/include/global/timings.h: 
+C:/D/SOFT/BUR\ One/BUR/include/global/structs.h: 
+C:/D/SOFT/BUR\ One/BUR/include/global/macro.h: 
+C:/D/SOFT/BUR\ One/BUR/include/global/hardware.h: 
+C:/D/SOFT/BUR\ One/BUR/include/chip/f280xbmsk.h: 
+C:/D/SOFT/BUR\ One/BUR/include/comm/serial_communication.h: 
+C:/D/SOFT/BUR\ One/BUR/include/comm/btwt41.h: 
+C:/D/SOFT/BUR\ One/BUR/include/global/interface.h: 
+C:/D/SOFT/BUR\ One/BUR/include/stat/LogEv.h: 
+C:/D/SOFT/BUR\ One/BUR/include/core/menu2x.h: 
+C:/D/SOFT/BUR\ One/BUR/include/core/menu_global.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/key_codes.h: 
+C:/D/SOFT/BUR\ One/BUR/include/core/value_conv.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/rtc.h: 
+C:/D/SOFT/BUR\ One/BUR/include/core/VlvDrvCtrl.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/Calibs.h: 
+C:/D/SOFT/BUR\ One/BUR/include/stat/im.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/rim_devices.h: 
+C:/D/SOFT/BUR\ One/BUR/include/stat/at25xxx.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/display.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/displhal.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/dac7513.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/encoder.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/adt7301.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/ds3234.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/pult.h: 
+C:/D/SOFT/BUR\ One/BUR/include/comm/Inputs.h: 
+C:/D/SOFT/BUR\ One/BUR/include/core/motor_control.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/ileg.h: 
+C:/D/SOFT/BUR\ One/BUR/include/core/sifu_calc.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/ApFilter1.h: 
+C:/D/SOFT/BUR\ One/BUR/include/peref/ApFilter3.h: 
+C:/D/SOFT/BUR\ One/BUR/include/core/protections.h: 
+C:/D/SOFT/BUR\ One/BUR/include/core/PrtCalc.h: 
+C:/D/SOFT/BUR\ One/BUR/include/comm/drv_modbus.h: 
+C:/D/SOFT/BUR\ One/BUR/include/comm/responses.h: 
+C:/D/SOFT/BUR\ One/BUR/include/core/params.h: 
+C:/D/SOFT/BUR\ One/BUR/include/global/system.h: 
+C:/D/SOFT/BUR\ One/BUR/include/core/protectionI2T.h: 
